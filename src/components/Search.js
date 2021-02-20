f530eaaf43d9dfaf97ab127693985f9ff63bcb9c
thedon122 <donovan122@yahoo.com>
2021-02-19 19:17:55 -0500

    added async

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
-
-  console.log('I RUN WITH EVERY RENDER');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log('I RUN AFTER EVERY RENDER AND AT INITIAL RENDER');
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -21,6 +46,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
