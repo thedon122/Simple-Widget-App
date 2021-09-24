0de33f00b93760044f69d18d4135f869a91eb28f
thedon122 <donovan122@yahoo.com>
2021-09-23 22:21:28 -0400

    search update

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,8 +20,23 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    if (term && !results.length){
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+      if (term) {
+      search();
+      } 
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+   }
   }, [term]);
+      
+    
+
 
   const renderedResults = results.map((result) => {
     return (
