37dfd793b92bbbe6136e8514bd338efec30f2319
thedon122 <donovan122@yahoo.com>
2021-09-26 23:58:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
+    title: "What is React?",
+    content: "React is a front end javascript framework",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
+    title: "How do you use React?",
+    content: "You use React by creating components",
   },
 ];
 
 const options = [
   {
-    label : 'The Color Red',
-    value: 'red'
+    label: "The Color Red",
+    value: "red",
   },
   {
-    label: 'The color Green',
-    value: 'green'
+    label: "The Color Green",
+    value: "green",
   },
   {
-    label: 'A Shade of Blue',
-    value: 'Bluex'
-  }
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ];
 
-export default () => {
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
+export default App;
