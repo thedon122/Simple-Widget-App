33df91eb824dc49263f91f966ee2c666cacc8e62
thedon122 <donovan122@yahoo.com>
2021-09-27 23:57:32 -0400

    update

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React from 'react';
 
-const Dropdown = ( { options } ) => {
-    const renderedOptions = options.map((option) =>{
-        return (
-            <div key={option.value} className="item">
-                {option.label}
-            </div>
-        );
-    }); 
-    return (<div className="ui form">
-        <div className="field">
-            <label className="label">
-                Select a color
-            </label>
-            <div className="ui selection dropdown visible active">
-                <i className="dropdown icon"></i>
-                <div className="text">Select Color</div>
-                <div className="menu visible transition">
-                    {renderedOptions}
-                </div>
-            </div>
-        </div>
-    </div>
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
     );
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
