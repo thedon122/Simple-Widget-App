4d144ba6a5313fc390901886e362e0434c6d9a7c
thedon122 <donovan122@yahoo.com>
2021-10-04 23:58:35 -0400

    update

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,7 @@
 import React, { useState}  from "react";
 import Dropdown from "./Dropdown";
 
+ //const api = AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM;
 const options = [
     {
         label: 'Afrikanns',
@@ -18,14 +19,22 @@ const options = [
 
 const Translate = () => {
     const [languge, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
+            <div className="ui form">
+                <div className="field">
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+            </div>
             <Dropdown 
+            label="Select a Language"
             selected={languge}
             onSelectedChange={setLanguage}
             options={options} 
             />
+                
         </div>
     );
 };
